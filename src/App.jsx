f78c318e8c6b6dc340b816a147b2a952ca3348d6
alifjobaer12 <alifjobaer12@gmail.com>
2026-01-17 10:14:32 +0600

    learn list with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Card from "./Card.jsx";
-import Info from "./info.jsx";
+import Info from "./Info.jsx";
 
 import profilePic from "./assets/My_Profile_Pic.jpg";
 
@@ -87,10 +87,7 @@ function App() {
 				/>
 			</div>
 
-
-			<Info information = {info} />
-
-
+			<Info information={info} />
 
 			<Footer />
 		</>
