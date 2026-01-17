f544dfa381de956fa91ba9a07550351bef886950
alifjobaer12 <alifjobaer12@gmail.com>
2026-01-17 10:17:56 +0600

    learn list with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Card from "./Card.jsx";
-import Info from "./Info.jsx";
+import Info from "./ProfileInfo.jsx";
 
 import profilePic from "./assets/My_Profile_Pic.jpg";
 
